717880b68d75f53179e50b4a8143beaa66da568b
iman0629 <imanali1823@gmail.com>
2026-03-15 14:20:16 +0500

    fix: root route using index for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
 
   <Route path="general" element={<GeneralModule />} />
   <Route path="about" element={<About />} />
--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <BrowserRouter basename="/FYP-InnerBloom">
+  <BrowserRouter basename="/FYP-InnerBloom/">
     <App />
   </BrowserRouter>
 );
